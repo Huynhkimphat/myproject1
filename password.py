a91d5b13915ba2943b5674de0d3fec819538993e
Huynhkimphat <hkimphat2012@gmail.com>
2020-02-03 15:29:25 +0700

    Update password.py

--- password.py
+++ password.py
@@ -1,5 +1,13 @@
 import random
-s = "abcdefghijklmnopqrstuvwxyz01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
-l = 8
-p =  "".join(random.sample(s,l ))
-print(p)
+s = "abcdefghijklmnopqrstuvwxyz"
+l = 2
+p=['0','0','0','0']
+p[0] =  "".join(random.sample(s,l ))
+s="01234567890"
+p[1] =  "".join(random.sample(s,l ))
+s="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+p[2] =  "".join(random.sample(s,l ))
+s="#!@#$%^&*()?"
+p[3]= "".join(random.sample(s,l ))
+for i in p:
+    print(i,end="")
